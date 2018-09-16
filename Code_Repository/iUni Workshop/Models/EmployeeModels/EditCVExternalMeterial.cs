0dfda330b201dc0e667c79d1fc647c9829a463b2
Kai Zhang <s3560808@student.rmit.edu.au>
2018-09-16 18:21:04 +1000

    Edit CV Update
    1. Rewrite Edit Cv in backend
    2. Add Validation of Edit CV
    3. Update cv find job status if date expire
    4. Update script for front end to add skill/external skill

--- Code_Repository/iUni Workshop/Models/EmployeeModels/EditCVExternalMeterial.cs
+++ Code_Repository/iUni Workshop/Models/EmployeeModels/EditCVExternalMeterial.cs
@@ -4,10 +4,10 @@ namespace iUni_Workshop.Models.EmployeeModels
 {
     public class EditCVExternalMeterial
     {
-        [Required]
-        public string ExternalMeterialName { get; set; }
+        [Required(ErrorMessage = "External material name is required!")]
+        public string ExternalMaterialName { get; set; }
         
-        [Required]
-        public string ExternalMeterialLink { get; set; }
+        [Required(ErrorMessage = "External material certification link is required!")]
+        public string ExternalMaterialLink { get; set; }
     }
 }
