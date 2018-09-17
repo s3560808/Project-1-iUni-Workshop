d426e9db3cdfda8842340719438a6eaebbb1b3b7
LevisSweet <38487306+LevisSweet@users.noreply.github.com>
2018-09-17 18:25:04 +1000

    Add files via upload

---
+++ MyMessages/js/no-javascript.js
@@ -0,0 +1,6 @@
+(function(e,t,n)
+	{
+		var r=e.querySelectorAll("html")[0];
+		r.className=r.className.replace(/(^|\s)n0-js(\s|$)/,"$1js$2")
+	})
+	(document,window,0);
---
+++ MyMessages/js/toggleSidebar.js
@@ -0,0 +1,5 @@
+function work(){
+	document.getElementById('sidebar').style.width = '200px';
+	document.getElementById('main').style.marginLeft = '200px';
+}
+	
